e73c2453d9d8fc137d4c5f03b122a522198b6498
Anderson Wang <shengminw@yahoo-inc.com>
2018-01-25 15:41:33 +0800

    Add bashrc and custom vim

--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -3,4 +3,6 @@ set -e
 
 cd ~/.vim_runtime
 cat ~/.vim_runtime/vimrcs/basic.vim > ~/.vimrc
+cat ~/.vim_runtime/bashrc/bashrc > ~/.bashrc
+cat ~/.vim_runtime/bashrc/bash_profile > ~/.bash_profile
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
